11ab1dfb99c793cc8bf1280dc8581677d817a0bc
Rutzno <d.rutzno@gmail.com>
2022-08-28 00:28:49 +0100

    initial commit

---
+++ hacking/hack.py
@@ -0,0 +1,27 @@
+import socket
+import argparse
+import sys
+
+# parser = argparse.ArgumentParser()
+args = sys.argv
+
+if len(args) != 4:
+    print("The script should be called with three arguments")
+else:
+    with socket.socket() as client_socket:
+        hostname = args[1]
+        port = int(args[2])
+        address = (hostname, port)
+        client_socket.connect(address)
+
+        data = args[3].encode()
+        client_socket.send(data)
+
+        # if data == "qwerty":
+        #     print("Connection Success!")
+        # else:
+        #     print("Wrong password!")
+        response = client_socket.recv(1024)
+        response = response.decode()
+
+        print(response)
