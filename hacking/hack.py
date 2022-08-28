f4f78620cbae823744398c052d12b7730eaccbc4
Rutzno <d.rutzno@gmail.com>
2022-08-28 01:03:21 +0100

    stage 1

--- hacking/hack.py
+++ hacking/hack.py
@@ -7,23 +7,24 @@ __since__ = "28/8/2021"
 __version__ = "1.0.1"
 """
 
-args = sys.argv
+if __name__ == "__main__":
+    args = sys.argv
 
-if len(args) != 4:
-    print("The script should be called with three arguments, the hostname, the port and the data to send")
-else:
-    with socket.socket() as client_socket:  # creating the socket with context manager
-        hostname = args[1]
-        port = int(args[2])
-        address = (hostname, port)
+    if len(args) != 4:
+        print("The script should be called with three arguments, the hostname, the port and the data to send")
+    else:
+        with socket.socket() as client_socket:  # creating the socket with context manager
+            hostname = args[1]
+            port = int(args[2])
+            address = (hostname, port)
 
-        client_socket.connect(address)  # connecting to the server
+            client_socket.connect(address)  # connecting to the server
 
-        data = args[3].encode()  # converting to bytes
+            data = args[3].encode()  # converting to bytes
 
-        client_socket.send(data)  # sending through socket
+            client_socket.send(data)  # sending through socket
 
-        response = client_socket.recv(1024)  # receiving the response
-        response = response.decode()  # decoding from bytes to string
+            response = client_socket.recv(1024)  # receiving the response
+            response = response.decode()  # decoding from bytes to string
 
-        print(response)
+            print(response)
